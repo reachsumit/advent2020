129718326199f8b005e8a949843dc8c6142543cd
Sumit :rocket <sam.sumitkumar@gmail.com>
2020-12-11 10:03:14 -0800

    Make day 11 solution a bit more concise

--- day11.py
+++ day11.py
@@ -1,18 +1,4 @@
-def count_adjacent(_seats, i, j):
-    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
-                  (0, 1), (1, -1), (1, 0), (1, 1)]
-    count = 0
-
-    for direction in directions:
-        new_i = i + direction[0]
-        new_j = j + direction[1]
-        if new_i >= 0 and new_j >= 0 and new_i < len(_seats)\
-                and new_j < len(_seats[0]) and _seats[new_i][new_j] == '#':
-            count += 1
-    return count
-
-
-def count_adjacent_q2(_seats, i, j):
+def count_adjacent(_seats, i, j, q1=True):
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
                   (0, 1), (1, -1), (1, 0), (1, 1)]
     count = 0
@@ -22,11 +8,12 @@ def count_adjacent_q2(_seats, i, j):
         while True:
             new_i += direction[0]
             new_j += direction[1]
-            if new_i >= 0 and new_j >= 0 and new_i < len(_seats)\
-                    and new_j < len(_seats[0]):
+            if 0 <= new_i < len(_seats) and 0 <= new_j < len(_seats[0]):
                 if _seats[new_i][new_j] != '.':
                     if _seats[new_i][new_j] == '#':
                         count += 1
+                    break
+                if q1:
                     break
             else:
                 break
@@ -42,34 +29,18 @@ def count_occupied(_seats):
     return count
 
 
-def day11q1(seats):
+def day11q1(seats, q1=True):
     seats_copy = [row[:] for row in seats]
+    max_occupied = 4 if q1 else 5
     something_changed = True
     while something_changed:
         something_changed = False
         for i in range(len(seats)):
             for j in range(len(seats[0])):
-                if seats[i][j] == 'L'and count_adjacent(seats, i, j) == 0:
+                if seats[i][j] == 'L'and count_adjacent(seats, i, j, q1) == 0:
                     something_changed = True
                     seats_copy[i][j] = '#'
-                elif seats[i][j] == '#' and count_adjacent(seats, i, j) >= 4:
-                    something_changed = True
-                    seats_copy[i][j] = 'L'
-        seats = [row[:] for row in seats_copy]
-    return count_occupied(seats)
-
-
-def day11q2(seats):
-    seats_copy = [row[:] for row in seats]
-    something_changed = True
-    while something_changed:
-        something_changed = False
-        for i in range(len(seats)):
-            for j in range(len(seats[0])):
-                if seats[i][j] == 'L'and count_adjacent_q2(seats, i, j) == 0:
-                    something_changed = True
-                    seats_copy[i][j] = '#'
-                elif seats[i][j] == '#' and count_adjacent_q2(seats, i, j) >= 5:
+                elif seats[i][j] == '#' and count_adjacent(seats, i, j, q1) >= max_occupied:
                     something_changed = True
                     seats_copy[i][j] = 'L'
         seats = [row[:] for row in seats_copy]
@@ -77,5 +48,7 @@ def day11q2(seats):
 
 
 all_seats = [[char for char in line.strip('\n')] for line in open('input.txt')]
+# Q1
 print(day11q1(all_seats))
-print(day11q2(all_seats))
+# Q2
+print(day11q1(all_seats, q1=False))
