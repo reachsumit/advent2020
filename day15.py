39c914572a23db5ab5c330b909002283bc28d023
Sumit :rocket <sam.sumitkumar@gmail.com>
2020-12-14 22:40:08 -0800

    quick optimizations

--- day15.py
+++ day15.py
@@ -1,26 +1,21 @@
-def day15q1(nums, q1=True):
+def day15q1(nums, target=True):
     memory = {}
     for turn, num in enumerate(nums):
+        memory[num] = (1, turn+1, 0)
+    for turn in range(len(nums)+1, target):
+        t, l1, l2 = memory[num]
+        if t == 1:
+            num = 0
+        else:
+            num = l1 - l2
         if num in memory:
             t, l1, l2 = memory[num]
-            memory[num] = (t+1, turn+1, l1)
+            memory[num] = (t + 1, turn, l1)
         else:
-            memory[num] = (1, turn+1, 0)
-    last = num
-    target = 2021 if q1 else 30000001
-    for turn in range(len(nums)+1, target):
-        if memory[last][0] == 1:
-            last = 0
-        else:
-            last = memory[last][1] - memory[last][2]
-        if last in memory:
-            t, l1, l2 = memory[last]
-            memory[last] = (t + 1, turn, l1)
-        else:
-            memory[last] = (1, turn, 0)
-    return last
+            memory[num] = (1, turn, 0)
+    return num
 
 
 all_nums = [int(num) for num in open('input.txt').read().split(',')]
-print(day15q1(all_nums))
-print(day15q1(all_nums, q1=False))
+print(day15q1(all_nums, target=2021))
+print(day15q1(all_nums, target=30000001))
